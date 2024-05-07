436dab509b7795a5ba6d2e988b65fb3e0ca70b85
Dzianis Mantsevich <dmantsevich@exadel.com>
2024-05-07 15:32:59 +0200

    Basic Structure

---
+++ index.js
@@ -0,0 +1,7 @@
+export * from './lib/setup';
+
+export * from './lib/decorators/component';
+export * from './lib/webcomponent';
+export * from './lib/webcomponent-extension';
+
+export * as DOMEvents from './lib/helpers/dom/events';
